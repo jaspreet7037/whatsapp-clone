adeeec10e49bd155f6a03113c992a85c8fc5df3d
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2023-07-07 13:05:44 +0530

    files added

---
+++ README.md
@@ -0,0 +1,5 @@
+# whatsapp-clone 🔥 🚀
+
+This project is made by Jaspreet Singh
+
+In this project, I have used MERN stack build to create WhatsApp-Clone Website(Full Stack Website) fully functional.
---
+++ client/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_wGCNPPArLSg6wHUjY9b-Yzp5vhiV1Qc",
+    authDomain: "whatsapp-clone-590c2.firebaseapp.com",
+    projectId: "whatsapp-clone-590c2",
+    storageBucket: "whatsapp-clone-590c2.appspot.com",
+    messagingSenderId: "355075199676",
+    appId: "1:355075199676:web:6d9c88c5394ef8867676b9"
+  };
