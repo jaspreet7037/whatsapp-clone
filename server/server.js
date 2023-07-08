5906c8acde6d888022b351222cfc8aea5ac52012
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2023-07-08 11:51:54 +0530

    server.js commented

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+//importing express module
 import express from 'express';
 
+//app config
 const app = express();
 const port = process.env.PORT || 9000;
 
+//api routes
 app.get("/", (req, res) => es.status(200).send("hello world!"));
 
-app.listen(port, () => console.log(`listening to port:${port}`));
+//listen
+app.listen(port, () => console.log(`listening on localhost:${port}`));
