4d8395684ec1d0f7121a09ac3ca06287670df5a8
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2023-08-10 04:09:20 +0530

    files updated

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 //importing express module
 import express from 'express';
+import mongoose from "mongoose"
 
 //app config
 const app = express();
 const port = process.env.PORT || 9000;
 
+// middleware 
+
+//DB config
+const connection_url = 'mongodb+srv://admin:B8IkQNFv1hf12UnX@cluster0.3hn1m09.mongodb.net/?retryWrites=true&w=majority';
+// mongodb+srv://admin:<password>@cluster0.3hn1m09.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(connection_url,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 //api routes
-app.get("/", (req, res) => es.status(200).send("hello world!"));
+app.get("/", (req, res) => res.status(200).send("hello world!"));
 
 //listen
 app.listen(port, () => console.log(`listening on localhost:${port}`));
